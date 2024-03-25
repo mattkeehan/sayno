dd5954ee3ee27273656c6ee8d2696dc25845984b
Matt Keehan <matt.keehan.ext@bbc.co.uk>
2024-03-25 13:35:33 +0000

    tidied

--- work.js
+++ work.js
@@ -1,6 +1,3 @@
-const sizes = ['28L', '77', '4', '8', '10S', '28R', '12', '24', '28S', '44L'];
-// const variants = ['S', 'R', 'L'];
-
 // order the list by sizes, with letter suffixes following their unsuffixed number - 
 // - eg., 28, 28S, 28M, 28L
 
@@ -10,9 +7,8 @@ const getOrderedSizes = (items) => {
       .replaceAll('S', '.1')
       .replaceAll('R', '.2')
       .replaceAll('L', '.3')
-  }).map(item => {
-    return Number(item);
-  }).sort((a, b) => {
+  }).map(item => Number(item)
+  ).sort((a, b) => {
     if (a < b) return -1;
     if (a > b) return 1;
     return 0;
@@ -22,7 +18,8 @@ const getOrderedSizes = (items) => {
       .replaceAll('.1', 'S')
       .replaceAll('.2', 'R')
       .replaceAll('.3', 'L')
-  })
+  });
 };
 
+const sizes = ['28L', '77', '4', '8', '10S', '28R', '12', '24', '28S', '44L'];
 console.log(getOrderedSizes(sizes));
